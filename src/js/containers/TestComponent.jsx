b657e1b99ec7e5ed103579dd5358cff4522215c3
Adam Morsi <adam.morsi@yahoo.com>
2021-12-16 21:42:35 +0200

    add tabs component

--- src/js/containers/TestComponent.jsx
+++ src/js/containers/TestComponent.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 //components
+import Tabs from '../components/shared/tabs/Tabs';
 
 const TestComponent = () => {
-	return <div style={{ padding: '4rem 0' }}>this is the second inner app</div>;
+	return (
+		<Tabs activeTab={0}>
+			<div label="First">First tab content</div>
+			<div label="Second">Second tab content</div>
+			<div label="Third">Third tab content</div>
+			<div label="Fourth">Fourth tab content</div>
+			<div label="Fifth">Fifth tab content</div>
+			<div label="Sixth">Sixth tab content</div>
+			<div label="Seventh">Seventh tab content</div>
+		</Tabs>
+	);
 };
 
 export default TestComponent;
